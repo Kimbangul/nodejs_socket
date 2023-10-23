b3d774644fb4252ad6d971529dba0a46c339d29d
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-10-23 16:41:19 +0900

    231023 node01

---
+++ node-01.js
@@ -0,0 +1,10 @@
+console.log(__filename);
+console.log(__dirname);
+
+for (var key in console) {
+  console.log(key);
+}
+
+console.log(process.env.HOME);
+console.log(process.versions.arch);
+console.log(process.arch);
